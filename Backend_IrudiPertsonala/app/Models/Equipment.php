0a7f67543b92873aaafe10d2d3566395e403bc91
Alejandro <arivass23@fpsanturtzilh.eus>
2026-01-15 10:53:38 +0100

    Modeloak restantes sortuta

--- Backend_IrudiPertsonala/app/Models/Equipment.php
+++ Backend_IrudiPertsonala/app/Models/Equipment.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Equipment extends Model
 {
-    
+    public function equipmentCategories(): BelongsToMany
+    {
+        return $this->belongsToMany(EquipmentCategorie::class);
+    }
 }
--- Backend_IrudiPertsonala/app/Models/EquipmentCategorie.php
+++ Backend_IrudiPertsonala/app/Models/EquipmentCategorie.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class EquipmentCategorie extends Model
 {
-    //
+    public function equipment(): HasMany
+    {
+        return $this->hasMany(Equipment::class);
+    }
 }
