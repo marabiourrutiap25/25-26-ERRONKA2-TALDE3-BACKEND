840b8d096f9f4c6253ed90cd15db9e5720bc944a
Alejandro <arivass23@fpsanturtzilh.eus>
2026-01-15 12:11:07 +0100

    N:M migrazioak eginda

---
+++ Backend_IrudiPertsonala/database/migrations/2026_01_15_110704_create_student_equipments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_equipments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained();
+            $table->foreignId('equipment_id')->constrained();
+            $table->dateTime('start_datetime');
+            $table->dateTime('end_datetime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_equipments');
+    }
+};
