a3cb0f21070066a51190192f0ce999328303c2f9
Oier <ogarciam23@fpsanturtzilh.eus>
2026-01-22 08:32:43 +0100

    Apia garatzen jarraitu.

--- Backend_IrudiPertsonala/routes/api.php
+++ Backend_IrudiPertsonala/routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AppointmentController;
 use App\Http\Controllers\Api\ConsumableCategoriController;
 use App\Http\Controllers\Api\ConsumableController;
 use App\Http\Controllers\Api\GroupController;
 use App\Http\Controllers\Api\ScheduleController;
+use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\ShiftController;
 use App\Http\Controllers\Api\StudentController;
+use App\Models\ServiceCategorie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +21,7 @@ Route::apiResource('consumable', ConsumableController::class);
 Route::apiResource('consumablecategori', ConsumableCategoriController::class);
 Route::apiResource('schedule', ScheduleController::class);
 Route::apiResource('student', StudentController::class);
-Route::apiResource('shift', ShiftController::class);
+Route::apiResource('shift',  ShiftController::class);
+Route::apiResource('appointment', AppointmentController::class);
+Route::apiResource('service', ServiceController::class);
+Route::apiResource('service-category', ServiceCategorie::class);
