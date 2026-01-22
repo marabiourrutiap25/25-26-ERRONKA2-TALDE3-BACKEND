d0ff84991fae69b8d2eda9edfd7bf2d9ce6e6fef
Alejandro <arivass23@fpsanturtzilh.eus>
2026-01-22 08:16:17 +0100

    API rutas commit

--- Backend_IrudiPertsonala/routes/api.php
+++ Backend_IrudiPertsonala/routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
-use App\Http\Controllers\Api\ConsumableCategoriController;
+use App\Http\Controllers\Api\AppointmentController;
+use App\Http\Controllers\Api\ClientController;
+use App\Http\Controllers\Api\ConsumableCategoryController;
 use App\Http\Controllers\Api\ConsumableController;
 use App\Http\Controllers\Api\GroupController;
 use App\Http\Controllers\Api\ScheduleController;
+use App\Http\Controllers\Api\ServiceCategoryController;
+use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\ShiftController;
 use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\EquipmentCategoryController;
+use App\Http\Controllers\EquipmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +21,13 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('group', GroupController::class);
 Route::apiResource('consumable', ConsumableController::class);
-Route::apiResource('consumablecategori', ConsumableCategoriController::class);
+Route::apiResource('consumable-category', ConsumableCategoryController::class);
 Route::apiResource('schedule', ScheduleController::class);
 Route::apiResource('student', StudentController::class);
 Route::apiResource('shift', ShiftController::class);
+Route::apiResource('equipment', EquipmentController::class);
+Route::apiResource('equipment-category', EquipmentCategoryController::class);
+Route::apiResource('appointment', AppointmentController::class);
+Route::apiResource('client', ClientController::class);
+Route::apiResource('service', ServiceController::class);
+Route::apiResource('service-category', ServiceCategoryController::class);
