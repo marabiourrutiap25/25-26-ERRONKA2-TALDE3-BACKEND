d5a4f25861c8bf7203dfee5b0d8231904635e97e
Alejandro <arivass23@fpsanturtzilh.eus>
2026-01-22 10:41:52 +0100

    Consumable en singular

--- Backend_IrudiPertsonala/database/migrations/2026_01_01_104054_create_consumable_categories_table.php
+++ Backend_IrudiPertsonala/database/migrations/2026_01_01_104054_create_consumable_categories_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('consumables_categories', function (Blueprint $table) {
+        Schema::create('consumable_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
@@ -22,6 +22,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('consumables_categories');
+        Schema::dropIfExists('consumable_categories');
     }
 };
