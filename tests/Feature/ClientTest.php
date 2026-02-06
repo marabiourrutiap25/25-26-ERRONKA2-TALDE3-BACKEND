9a43c52b3f6807420bf27ebd654f296d74bd47bf
Oier <ogarciam23@fpsanturtzilh.eus>
2026-02-06 12:26:50 +0100

    NaDa

--- tests/Feature/ClientTest.php
+++ tests/Feature/ClientTest.php
@@ -4,6 +4,21 @@ use App\Models\Client;
 
 use function Pest\Laravel\postJson;
 
+// Get All
+test('Get all Clients erantzun egokia bueltatzen du', function () {
+    $estructura = [
+        'success',
+        'data' => [
+            '*' => ['id', 'name', 'surnames', 'telephone', 'email', 'home_client', 'created_at', 'updated_at', 'deleted_at']
+        ]
+    ];
+
+    Client::factory()->count(3)->create();
+
+    $response = $this->getJson('api/clients');
+    $response->assertStatus(200);
+    $response->assertJson([
+        'success' => true,
     ]);
     $response->assertJsonCount(3, 'data');
     $response->assertJsonStructure($estructura);
